d703891ac6e7b45f2dd8fe16fb7eb3e55d2277fd
jaycode <luckyacho2@gmail.com>
2024-11-29 04:36:20 -0800

    finishing up pages

--- React-Django/frontend/src/Components/Pages/ContactUs.jsx
+++ React-Django/frontend/src/Components/Pages/ContactUs.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import { FaFacebook, FaWhatsapp, FaTiktok, FaPhone } from "react-icons/fa6";
+import Button from "../Utilities/Button";
 export const ContactUs = () => {
   return (
-    <div>ContactUs</div>
-  )
-}
+    <div className="contact-section">
+      <div className="social-links">
+        <div className="heading">Follow us</div>
+        <div className="social-media">
+          <FaFacebook /> <FaTiktok /> <FaWhatsapp /> <FaPhone />
+        </div>
+      </div>
+
+      <div className="contact-form">
+        <form onSubmit={handleOnSubmit}>
+          <Button type="submit" title="submit" />
+        </form>
+      </div>
+    </div>
+  );
+};
