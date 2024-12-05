fc68b690fde1a65141e4ed9f89fa31079d2776f9
jaycode <luckyacho2@gmail.com>
2024-12-05 10:37:54 -0800

    frontend update

--- React-Django/backend/api/models.py
+++ React-Django/backend/api/models.py
@@ -1,7 +1,29 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-class Item(models.Model):
-    title = models.CharField(max_length=100)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+
     def __str__(self):
-        return self.title
+        return self.user.username
+
+class course(models.Model):
+    subject = models.CharField(max_length=100)
+    test_score = models.CharField(max_length=100)
+    exam_score = models.CharField(max_length=100)
+    total_score = models.CharField(max_length=100)
+    grade = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.subject
+
+
+class Scratch_card(models.Model):
+    card_number = models.CharField(max_length=100)
+    is_valid = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.card_number
