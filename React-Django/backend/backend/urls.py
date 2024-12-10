25e80f98f1776d9a9625be549ecc0342c64a8372
jaycode <luckyacho2@gmail.com>
2024-12-09 23:14:30 -0800

    index route addition

--- React-Django/backend/backend/urls.py
+++ React-Django/backend/backend/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from api.views import UserRegistration
+from django.http import JsonResponse
+
+def index(response):
+    return JsonResponse({'msg': 'Welcome to gradepoint api'})
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index(), name='index'),
     path('api/', include('api.urls')),
     path('register-user/', UserRegistration.as_view(), name='register')
 ]
