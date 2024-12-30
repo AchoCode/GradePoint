5fa9b25403cc45cd95086e6235ea4db02c1e7ade
jaycode <luckyacho2@gmail.com>
2024-12-30 15:17:41 -0800

    migration file edit

--- React-Django/backend/api/migrations/0014_create_superuser_2.py
+++ React-Django/backend/api/migrations/0014_create_superuser_2.py
@@ -3,9 +3,19 @@ from django.db import migrations
 import os
 
 def create_superuser(apps, schema_editor):
-    Ufilter(email=email).delete()
+    User = apps.get_model('auth', 'User')
+    UserProfile = apps.get_model('api', 'UserProfile')  # Adjust the import path
+    username = os.getenv('DJANGO_ADMIN_USERNAME')
+    email = os.getenv('DJANGO_ADMIN_EMAIL')
+    password = os.getenv('DJANGO_ADMIN_PASSWORD')
 
-    # Create the new superuser
+    if not all([username, email, password]):
+        raise ValueError(
+            "One or more environment variables (DJANGO_ADMIN_USERNAME, DJANGO_ADMIN_EMAIL, DJANGO_ADMIN_PASSWORD) are missing."
+        )
+
+    # Delete any existing superuser with the same username or email
+    User.objects.filter(username=username).delete()
     user = User.objects.create_superuser(
         username=username,
         email=email,
