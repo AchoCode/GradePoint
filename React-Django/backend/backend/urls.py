bd931fdc5dce52478d1168729138eefd60e156de
jaycode <luckyacho2@gmail.com>
2025-02-25 01:55:34 -0800

    ping database api added

--- React-Django/backend/backend/urls.py
+++ React-Django/backend/backend/urls.py
@@ -3,7 +3,26 @@ from django.urls import path, include
 from api.views import UserRegistration
 from django.http import JsonResponse
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+import sqlite3
+import os
+DB_PATH = os.getenv("DB_PATH", "db.sqlite3")
 
+def ping_db(request):
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("SELECT 1;")  # Simple query
+        cursor.close()
+        conn.close()
+        print('''
+        =============================
+        CRON-JOB EXECUTED
+        =============================
+        ''')
+        return JsonResponse({"status": "Database pinged successfully"}, status=200)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
+    
 def index(response):
     return JsonResponse({'msg': 'Welcome to gradepoint api'})
 
@@ -15,5 +34,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
     path('api-auth/', include('rest_framework.urls')),
+    path('ping-db', ping_db, name='ping')
 
 ]
